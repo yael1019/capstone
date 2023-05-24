77659f1e4da16028e33ff38ecdee8230e2ae26f2
Yael Harosh <yaelharosh01@gmail.com>
2023-05-24 17:16:09 -0400

    appointment styles

--- client/screens/AppointmentCard.js
+++ client/screens/AppointmentCard.js
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import React from 'react'
 import {useNavigation} from '@react-navigation/native'
 
@@ -6,12 +6,38 @@ const AppointmentCard = ({apts}) => {
     const navigation = useNavigation()
   return (
     <View>
-        <TouchableOpacity onPress={() => navigation.navigate('AppointmentPage', {apts: apts})}>
-            <Text>Appointment</Text>
-            <Text>{ apts.service }</Text>
+        <TouchableOpacity style={styles.item} onPress={() => navigation.navigate('AppointmentPage', {apts: apts})}>
+            {/* <Text>Appointment</Text> */}
+            <Text style={styles.text}>{ apts.service }</Text>
         </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: '#fff',
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: 'row',
+    // alignItems: 'center',
+    marginBottom: 20,
+    marginLeft: 15,
+    marginRight: 15,
+    shadowOffset: {
+        width: 0,
+        height: 6,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 16.0,
+    // elevation: 24,
+    // borderBottomColor: '#ea580c',
+    // borderBottomWidth: 1.5
+  },
+  text: {
+  //    maxWidth: '80%' 
+      fontSize: 16
+  },
+})
 
 export default AppointmentCard
--- client/screens/AppointmentPage.js
+++ client/screens/AppointmentPage.js
@@ -1,17 +1,109 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Dimensions, ImageBackground, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const AppointmentPage = ({ navigation, route }) => {
     const {apts} = route.params
+    // console.log(apts)
   return (
-    <View>
-      <Text>AppointmentPage</Text>
-      <Text>{ apts.service }</Text>
-      <Text>{ apts.specialist }</Text>
-      <Text>{ apts.date }</Text>
-      <Text>{ apts.time }</Text>
+    <View style={styles.container}>
+      <ImageBackground source={require('../orange.jpeg')} style={styles.image}>
+          <TouchableOpacity>
+          <Text onPress={() => navigation.goBack()} style={styles.arrow}>←</Text>
+          </TouchableOpacity>
+          <Text style={styles.brandViewText}>Appointment</Text>
+      </ImageBackground>
+
+      <View style={styles.viewContainer}>
+        <View style={styles.textContainer}>
+          {/* <Text>AppointmentPage</Text> */}
+          <Text>
+            <Text style={{fontWeight: 'bold', fontSize: 22,}}>Service: </Text>
+            <Text style={styles.text}>{ apts.service }</Text>
+          </Text>
+          <Text>
+            <Text style={{fontWeight: 'bold', fontSize: 22,}}>Specialist: </Text>
+            <Text style={styles.text}>{ apts.specialist }</Text>
+          </Text>
+          <Text>
+          <Text style={{fontWeight: 'bold', fontSize: 22, }}>Date: </Text>
+          <Text style={styles.text}>{ apts.date }</Text>
+          </Text>
+          <Text>
+          <Text style={{fontWeight: 'bold', fontSize: 22, }}>Time: </Text>
+          <Text style={styles.text}>{ apts.time }</Text>
+          </Text>
+        </View>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    // height: Dimensions.get('window').he
+},
+arrow: {
+  fontSize: 30,
+  marginTop: 55,
+  marginLeft: 10,
+},
+image: {
+  height: Dimensions.get('window').height / 5.5
+},
+brandViewText: {
+  color: '#ffffff',
+  fontSize: 20,
+  fontWeight: 'bold',
+  alignSelf: 'center',
+  marginTop: -20,
+  textTransform: 'uppercase'
+},
+viewContainer: {
+  // backgroundColor: '#ffffff',
+  backgroundColor: '#fff',
+  flex: 1.5,
+  justifyContent: 'center',
+  // alignItems: 'center',
+  borderTopStartRadius: 60,
+  borderTopEndRadius: 60,
+  bottom: 60,
+  // height: Dimensions.get('window').height ,
+  paddingTop: 30,
+  paddingLeft: 10,
+  paddingRight: 10,
+  justifyContent: 'flex-start',
+  // paddingTop: 20
+},
+textContainer: {
+  // marginBottom: 20
+  borderWidth: 6,
+  // borderColor: '#ea580c',
+  borderColor: '#fb923c',
+  // borderColor: 'black',
+  // backgroundColor: '#E8EAED',
+  // backgroundColor: '#fff',
+  // backgroundColor: '#ea580c',
+  backgroundColor: '#f5f5f5',
+  // backgroundColor: 'black',
+  // backgroundColor: '#fb923c',
+  borderRadius: 30,
+  padding: 40,
+  marginTop: 50,
+  marginLeft: 10,
+  marginRight: 10,
+  flex: 0.8,
+  justifyContent: 'space-evenly',
+  shadowOffset: {width: 5, height: 10},
+  shadowOpacity: 0.6,
+  shadowRadius: 3,
+},
+text: {
+  fontSize: 20,
+  padding: 5, 
+  // color: '#fff'
+},
+})
 
 export default AppointmentPage
